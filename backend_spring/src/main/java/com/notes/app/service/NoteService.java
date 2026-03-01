49c56c28a65f67967c8dbd61d8de6d1f58291a82
eshanhaq4 <59237231+eshanhaq4@users.noreply.github.com>
2026-02-28 20:03:27 -0600

    Edited NoteService.java to match the new proto and tested/debugged some system issues I was having.

--- backend_spring/src/main/java/com/notes/app/service/NoteService.java
+++ backend_spring/src/main/java/com/notes/app/service/NoteService.java
@@ -4,18 +4,22 @@ import com.notes.app.data.Note;
 import com.notes.app.data.NoteRepository;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
+import com.notes.app.grpc.NoteSummaryEvent;
 
 @Service
 public class NoteService {
   private static final String NOTE_SUMMARY_EVENT_QUEUE = "note_summary_event_queue";
 
   private final NoteRepository noteRepository;
-  private final StringRedisTemplate redis;
+  private final RedisTemplate<String, byte[]> redis;
 
-  public NoteService(NoteRepository noteRepository, StringRedisTemplate redis) {
+  public NoteService(NoteRepository noteRepository, RedisTemplate<String, byte[]> redis) {
     this.noteRepository = noteRepository;
     this.redis = redis;
   }
@@ -32,9 +36,14 @@ public class NoteService {
     // validate content and color
     Note note = noteRepository.save(new Note(content, color));
 
-    String event = note.getId() + "::" + content;
-    redis.opsForList().rightPush(NOTE_SUMMARY_EVENT_QUEUE, event);
+    NoteSummaryEvent event = NoteSummaryEvent.newBuilder()
+        .setEventId(UUID.randomUUID().toString())
+        .setNoteId(note.getId().toString())
+        .setContent(content)
+        .setTimestamp(System.currentTimeMillis())
+        .build();
 
+    redis.opsForList().rightPush(NOTE_SUMMARY_EVENT_QUEUE, event.toByteArray());
     System.out.println("NoteService.java: Queued summary job for note: " + note.getId());
 
     return note;
